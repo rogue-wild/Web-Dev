7014e67d7b137bf6681c3bba81a386181f663b80
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2023-03-03 02:06:45 +0530

    update

---
+++ intro-to-node/index.js
@@ -0,0 +1,11 @@
+//jshint esversion:6
+
+// const fs = require("fs"); 
+
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+var superheroes = require("superheroes");
+
+var myName = superheroes.random();
+
+console.log(myName);
