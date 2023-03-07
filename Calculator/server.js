cecba12f7db48065957e17b42f3a1771a4043cdb
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2023-03-07 17:15:44 +0530

    adding new projects

---
+++ Calculator/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/bmiCalculator", function(request, response){
+    //  response.sendFile('X:/Vishnu/Web Dev/Calculator/index.html');
+    response.sendFile(__dirname + "/bmiCalc.html");
+});
+
+
+app.get("/calculator", function(request, response){
+    //  response.sendFile('X:/Vishnu/Web Dev/Calculator/index.html');
+    response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/calculator", function(request, response){
+    var num1 =  Number(request.body.weight);
+    var num2 = Number(request.body.height);
+
+    // var result = num1+num2;
+//    var result = num1/(num2*num2);
+//    result = Math.round(result);
+
+    response.send("Result: " + result);
+})
+
+app.post("/bmiCalculator", function(request, response){
+    var num1 =  parseFloat(request.body.weight);
+    var num2 = parseFloat(request.body.height);
+
+    // var result = num1+num2;
+   var result = num1/(num2*num2);
+   result = Math.round(result);
+
+    response.send("Result: " + result);
+})
+
+app.listen(3000, function(){
+    console.log("Server On");
+});
--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,6 +1,21 @@
+
+const { response } = require("express");
 const express = require("express");
 
 const app = express();
+
+app.get("/", function(request, response){
+   response.send("<h1>Hello, World!</h1><a href='/contact'>contact</a><br><a href='/about-me'>About me</a>");
+});
+// when the app.get gets a request at home location("/") then the callback gets triggered and we send the response which the plain text of hello.
+app.get("/contact", function(request, response){
+    response.send("contact me at vb007@gmail.com<br><a href='/'>Back</a>");
+});
+
+app.get("/about-me", function(request, response){
+    response.send("<h1>About</h1><p>Hello, this Vishnu Bhagwat</p><br><a href='/'>Back</a>")
+})
+
 
 app.listen(3000, function() {
     console.log("Server started at 3000");
