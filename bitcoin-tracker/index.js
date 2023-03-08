8c87facf14783ab49118523adfb7c1cb2acd5a9f
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2023-03-09 01:51:47 +0530

    .

--- bitcoin-tracker/index.js
+++ bitcoin-tracker/index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
+
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function(){
-   console.log("Starting Server!");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+
+request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body){
+  console.log(response.statusCode);
+});
+
+});
+
+app.listen(3000, function () {
+  console.log("Starting Server!");
 });
