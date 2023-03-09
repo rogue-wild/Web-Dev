9632f5eeb729fa4913e7238e15d781a7a447d76d
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2023-03-10 00:25:09 +0530

    .

--- bitcoin-tracker/index.js
+++ bitcoin-tracker/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,11 +11,39 @@ app.get("/", function (req, res) {
 });
 
 app.post("/", function (req, res) {
+  var crypto = req.body.crypto;
+  var fiat = req.body.fiat;
+  var amount = req.body.amount;
 
-request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function(error, response, body){
-  console.log(response.statusCode);
-});
+  var public_key = "NDg0MzE2MmRjYTVjNDEzMTlhNGE0MjQ5MTVmYzg1NGU";
+  var baseURL = "https://apiv2.bitcoinaverage.com/convert/global";
 
+  var options = {
+    url: baseURL,
+    method: "GET",
+    headers: {
+      "x-ba-key": public_key,
+    },
+    qs: {
+      from: crypto,
+      to: fiat,
+      amount: amount,
+    },
+  };
+
+  request(options, function (error, response, body) {
+    var data = JSON.parse(body);
+    var price = data.price;
+    var currentDate = data.time;
+
+    res.write("<p>The date is " + currentDate + "</p>");
+
+    res.write(
+      "<h1>Price of " + amount + " " + crypto + " is: " + price + fiat + "</h1>"
+    );
+
+    res.send();
+  });
 });
 
 app.listen(3000, function () {
