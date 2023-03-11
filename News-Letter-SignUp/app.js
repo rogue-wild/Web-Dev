1a830995cc5e7d11e174bdd8748debfbf13bb854
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2023-03-12 01:55:11 +0530

    .

--- News-Letter-SignUp/app.js
+++ News-Letter-SignUp/app.js
@@ -18,9 +18,42 @@ app.post("/", function (req, res) {
   var firstName = req.body.fName;
   var lastName = req.body.lName;
   var email = req.body.email;
-  
-  console.log(firstName, lastName, email);
 
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us11.api.mailchimp.com/3.0/lists/715e1f9b57",
+    method: "POST",
+    headers: {
+      Authorization: "vishnu1 10b9ccbc94021c4880b7ac93489f0bb3-us11",
+    },
+    body: jsonData,
+  };
+
+  request(options, function (error, response, body) {
+    if (error || response.statusCode != 200) {
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      res.sendFile(__dirname + "/success.html");
+    }
+  });
+});
+
+app.post("/failure", function (req, res) {
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
