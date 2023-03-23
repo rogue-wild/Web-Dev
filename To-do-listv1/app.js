e72e1b3e09c0a5365c550c92044934eec8bd11c6
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2023-03-24 00:02:16 +0530

    .

--- To-do-listv1/app.js
+++ To-do-listv1/app.js
@@ -5,22 +5,33 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = [];
+
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long",
   };
 
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
 
   res.render("list", {
     kindOfDay: day,
+    newListItems: items,
   });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
