59a3d78228b157815130fdfb81e782895839b5bb
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2023-04-26 14:04:40 +0530

    .

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -1,21 +1,35 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const { MongoClient } = require('mongodb');
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'myproject';
-
-// Create a new MongoClient
 const client = new MongoClient(url);
 
-// Use connect method to connect to the Server
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+// Database Name
+const dbName = 'fruitsDB';
 
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log('Connected successfully to server');
   const db = client.db(dbName);
+  const collection = db.collection('fruits');
 
-  client.close();
-});
+  // Insert some documents about fruits
+  const insertResult = await collection.insertMany([
+    { name: 'Apple', score: 8, review: 'Great fruit' },
+    { name: 'Orange', score: 6, review: 'Kinda sour' },
+    { name: 'Banana', score: 9, review: 'Great stuff!' }
+  ]);
+  console.log('Inserted documents =>', insertResult);
+
+  // Query the collection to see the fruits
+  const fruits = await collection.find({}).toArray();
+  console.log('Fruits in the collection =>', fruits);
+
+  return 'done.';
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
