70a3fd6d5cdef261e25baf26c28737e8950292ec
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2023-06-06 02:06:56 +0530

    .

--- Understanding TypeScript/section-14/src/App.tsx
+++ Understanding TypeScript/section-14/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
-import { Todo } from "./todo.model";
+
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<{ id: string; text: string }[]>([]);
