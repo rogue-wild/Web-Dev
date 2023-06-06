f13c22db0464eb625e16db4cb40c0e1158ad83ba
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2023-06-07 00:47:58 +0530

    adding axios

--- Understanding TypeScript/section-14/src/components/NewTodo.tsx
+++ Understanding TypeScript/section-14/src/components/NewTodo.tsx
@@ -1,17 +1,35 @@
- import React, { useRef } from "react";
- import "./NewTodo.css"
+import React, { useRef } from "react";
+import axios from 'axios';
+import "./NewTodo.css"
 
 type NewTodoProps = {
   onAddTodo: (todoText: string) => void;
+  onAddList: (list: { id: string, name: string }) => void;
 };
 
-const NewTodo: React.FC<NewTodoProps> = props => {
+const NewTodo: React.FC<NewTodoProps> = (props) => {
   const textInputRef = useRef<HTMLInputElement>(null);
 
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     const enteredText = textInputRef.current!.value;
     props.onAddTodo(enteredText);
+
+    axios.post('http://localhost:3000/lists', {
+      name: enteredText,
+      colorId: 3
+    })
+      .then(function (response) {
+        props.onAddList(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    // Clear input field after submitting
+    if (textInputRef.current) {
+      textInputRef.current.value = '';
+    }
   };
 
   return (
@@ -20,6 +38,7 @@ const NewTodo: React.FC<NewTodoProps> = props => {
         <input type="text" name="" id="todo-text" ref={textInputRef} />
       </div>
       <button type="submit">ADD TODO</button>
+      {/* <button type="submit">ADD LIST</button> */}
     </form>
   );
 };
