7efa9928fd91efa175f7df4bc84302f9715c36cd
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2023-11-30 11:29:39 +0530

    .

---
+++ JS_DSA/ChaiCode/prevSection/variable.js
@@ -0,0 +1,8 @@
+const accountId = 8371983;
+let accountEmail = "vishnu@gmail.com"
+var accountPassword = "43124"
+accountCity = "Jaipur"
+
+//accountId = 2 // not allowed because it is a constant 
+
+console.log(accountId);
