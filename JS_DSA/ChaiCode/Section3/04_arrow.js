dd3b5f8871755ca50dcca49e75e29fef4e4d7faf
Vishnu Bhagwat <vishnu.bhagwat007@gmail.com>
2023-12-05 15:22:09 +0530

    Refactor arrow function and remove commented code

--- JS_DSA/ChaiCode/Section3/04_arrow.js
+++ JS_DSA/ChaiCode/Section3/04_arrow.js
@@ -1,8 +1,29 @@
-const user = {
-  username: "hitesh",
-  price: 999,
+// const user = {
+//   username: "hitesh",
+//   price: 999,
 
-  welcomeMessage: function () {
-    console.log(`${this.username}, welcome to website`);
-  },
-};
+//   welcomeMessage: function () {
+//     console.log(`${this.username}, welcome to website`);
+//     console.log(this);
+//    here this talks about the current context of variable
+//           {
+//           username: 'hitesh',
+//           price: 999,
+//           welcomeMessage: [Function: welcomeMessage]
+//            }
+//   },
+// };
+
+// user.welcomeMessage();
+// user.username = "sam";
+// user.welcomeMessage();
+// console.log(this); //output: {}
+
+// function chai() {
+//   console.log(this)//print many things;
+
+// const username = "raju";
+// console.log(this.username);//undefined
+// } 
+
+// chai();
